3b0934372c039f0f400d3c029414c2ec9ae42fb1
Sofiannedeer <ojingirihannah22@gmail.com>
2023-10-19 18:43:52 -0700

    another test

--- main.c
+++ main.c
@@ -8,11 +8,30 @@
  */
 ssize_t read_command(char **line, size_t *n)
 {
-	char *request = "$ ";
-	size_t read;
+	int is_interactive = isatty(STDIN_FILENO);
+	char *request;
+	ssize_t read;
 
-	write(1, request, strlen(request));
+	if (is_interactive)
+	{
+		request = "$ ";
+		write(1, request, strlen(request));
+	}
 	read = getline(line, n, stdin);
+
+	if (!is_interactive && read == -1)
+	{
+		return (-1);
+	}
+	if (is_interactive && read == -1)
+	{
+		return (-1);
+	}
+	if (is_interactive && read > 0 && (*line)[read - 1] == '\n')
+	{
+		(*line)[read - 1] = '\0';
+		read--;
+	}
 
 	return (read);
 }
@@ -101,11 +120,12 @@ int main(int arc, char **argv)
 		{
 			break;
 		}
-		else
+		if (msg[0] == '#' || (msg[0] == ' ' && msg[1] == '#'))
 		{
-			split_string(msg);
+			continue;
 		}
+		split_string(msg);
 	}
 	free(msg);
-	return (0);
+	return (EXIT_SUCCESS);
 }
